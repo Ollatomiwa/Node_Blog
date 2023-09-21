34d4ae449ca70bfc62ef9dba1fdb85ac5693c8e2
ollatomiwa <ajaomoshood@gmail.com>
2023-09-21 21:48:37 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+require("dotenv").config();
+
+
+const app = express();
+const PORT = 3000 || process.env.PORT;
+
+app.use(express.static('public'));
+
+//Templating Engine  //MiddleWares
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./server/routes/main'));
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
