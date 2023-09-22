8734f40d0d2a154e5f2f2e736d972b84efd66275
ollatomiwa <ajaomoshood@gmail.com>
2023-09-22 23:57:29 +0100

    second commit: our page is setting up with headers

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,8 +4,19 @@ const router = express.Router();
 
 //Routes
 router.get('', (req,res) => {
-    res.send("Hello World");
+    const locals = {
+        title: "NodeJs Blog",
+        description: "Simple Blog created with NodeJS, Express & MongoDB."
+    }
+
+    res.render('index', { locals });
 });
+
+
+router.get('about', (req, res) => {
+    res.sender('about');
+});
+
 
 
 module.exports = router;
