f7c56a472e532a47e2183c120eb81ecff88eaaa7
ollatomiwa <ajaomoshood@gmail.com>
2023-09-23 12:58:20 +0100

    mongodb database connected to our code

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
-require("dotenv").config();
 
+const connectDB = require('./server/config/db');
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
+
+//connect to DB
+connectDB();
 
 app.use(express.static('public'));
 
